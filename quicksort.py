9e3eacd3464edefcf67b6204897acbbd56111cad
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-03-30 15:51:19 -0400

    added quicksort

---
+++ quicksort.py
@@ -0,0 +1,15 @@
+import random
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+random.seed(42)
+new_array = random.sample(range(1, 11), 10)
+print(quicksort(new_array))
