2ebbc6e7e36e1497cab838fc771da3949d254881
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-03-30 14:20:33 -0400

    added selection sort

---
+++ selectionsort.py
@@ -0,0 +1,22 @@
+import random
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selection_sort(arr):
+    sorted_array = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        sorted_array.append(arr.pop(smallest))
+    return sorted_array
+
+random.seed(42)
+new_array = random.sample(range(1, 30), 20)
+
+print(selection_sort(new_array))
